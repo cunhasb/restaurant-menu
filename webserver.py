752802cafd1908bebb786a7157b8c5e7ab92c25a
Fabiano Cunha <cunhasb@aol.com>
2018-01-31 16:41:40 -0500

    create different routes, form and post request

--- webserver.py
+++ webserver.py
@@ -1,4 +1,5 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
 
 
 class webserverHandler(BaseHTTPRequestHandler):
@@ -10,15 +11,50 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.end_headers()
 
                 output = ""
-                output += '<html><body>Hello from Python!</body></html>'
+                output += "<html><body><h1>Hello from Python!<a href='/holla'>Spanish</a></h1>"
+                output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text'> <input type='submit' value='Submit'></form>"
+                output += "</body></html>"
                 self.wfile.write(output)
                 print output
                 return
 
+            if self.path.endswith('/holla'):
+                self.send_response(200)
+                self.send_header('Content_type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body><h1>&#161Hola de Python!<a href = '/hello'>Regressa a hello</a></h1></body></html>"
+                output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text'> <input type='submit' value='Submit'></form>"
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
         except IOError:
             print '404, This is embarrassing, I could not find this page! File not Found %s - ' % self.path
             self.send_error(
                 404, "This is embarrassing, I could not find this page! File not Found - File Not Found %s" % self.path)
+
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            self.end_headers()
+
+            ctype, pdict = cgi.parse_header(
+                self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+            output = ""
+            output += "<html><body>"
+            output += "<h2>Okay,how about this: </h2>"
+            output += "<h1>%s</h1>" % messagecontent[0]
+            output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text'> <input type='submit' value='Submit'></form>"
+            output += "</body></html>"
+            self.wfile.write(output)
+            print output
+        except IOError:
+            pass
 
 
 def main():
